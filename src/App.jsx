41cbc4e3508e7571195a8843c804a28ed7fd6af1
coderdal <agircoder@gmail.com>
2023-07-12 22:03:04 +0300

    react router install & setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+//layouts
+import UserLayout from "@/layouts/user.jsx";
+
+//Pages
+import Home from "@/pages/Home.jsx";
+
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<UserLayout />}>
+        <Route path="" element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
 
